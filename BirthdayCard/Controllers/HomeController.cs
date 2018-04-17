c015788b09e9f6beba8c54a7e0f94f8bd39b701e
crferb <crferb@gmail.com>
2018-04-16 18:36:00 -0700

    added homework1

---
+++ BirthdayCard/Controllers/HomeController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace BirthdayCard.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public ActionResult CardForm()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult CardForm(Models.CardContents cardContents)
+        {
+            if (ModelState.IsValid)
+                return View("Card", cardContents);
+            else
+                return View();
+        }
+    }
+}
